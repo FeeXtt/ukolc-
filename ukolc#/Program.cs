4a126a5ddc1d184f9a6a5fca8fb83913cd9d3c05
Stanislav Cinkl <stanislav.cinkl@student.smspmb.cz>
2024-09-09 18:43:05 +0200

    Přidejte soubory projektu.

---
+++ ukolc#/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using ukolc_;
+
+namespace SimpleEnemyFight{
+    public class Program {
+        static void Main(string[] args) {
+            Enemy franta = new Enemy("Franta", 5, 30);
+            Enemy pepa = new Enemy("Pepa", 3, 40);
+
+            franta.Attack(pepa);
+            pepa.Attack(franta);
+
+            Console.WriteLine(franta);
+            Console.WriteLine(pepa);
+        }
+    }
+}
---
+++ ukolc#/entity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ukolc_
+{
+    class Entity
+    {
+        public string Name { get; set; }
+
+        public Entity(string name)
+        {
+            Name = name;
+        }
+    }
+
+    class Enemy : Entity
+    {
+        public int BaseDamage { get; set; }
+        public int Hp { get; set; }
+
+        public Enemy(string name, int baseDamage, int hp) : base(name)
+        {
+            BaseDamage = baseDamage;
+            Hp = hp;
+        }
+
+        public void Attack(Enemy enemy)
+        {
+            enemy.Hp -= this.BaseDamage;
+            Console.WriteLine($"{this.Name} zautocil {enemy.Name}, nyni {enemy.Name} ma {enemy.Hp} Hp");
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} BaseDamage: {BaseDamage} Hp: {Hp}";
+        }
+    }
+
+}
